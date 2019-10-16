72e786e8c80bced80dec8cf7f0caf77c393e10ca
sazzad <sazzad@hsenidmobile.com>
2019-10-16 10:35:16 +0600

    Swagger documentation added

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+val swaggerVersion = "2.9.2"
+val jjwtVersion = "0.9.1"
+
 plugins {
 	id("org.springframework.boot") version "2.1.9.RELEASE"
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
@@ -18,14 +21,18 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
+//	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("io.springfox:springfox-swagger2:$swaggerVersion")
+	implementation("io.springfox:springfox-swagger-ui:$swaggerVersion")
+	implementation("io.jsonwebtoken:jjwt:$jjwtVersion")
+
 	runtimeOnly("com.h2database:h2")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+//	testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<KotlinCompile> {
--- src/main/kotlin/com/lynas/springsecurityjwt/SpringSecurityJwtApplication.kt
+++ src/main/kotlin/com/lynas/springsecurityjwt/SpringSecurityJwtApplication.kt
@@ -2,10 +2,73 @@ package com.lynas.springsecurityjwt
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import springfox.documentation.builders.ParameterBuilder
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.schema.ModelRef
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Parameter
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.*
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import springfox.documentation.annotations.ApiIgnore
+import springfox.documentation.service.Contact
+
 
 @SpringBootApplication
-class SpringSecurityJwtApplication
+@EnableSwagger2
+class SpringSecurityJwtApplication {
+
+    @Bean
+    fun docket(): Docket {
+        val parameter = ParameterBuilder()
+                .name("Authorization")
+                .description("Authorization token required for requesting secured content")
+                .modelRef(ModelRef("string"))
+                .parameterType("header")
+                .required(false)
+                .build()
+        val parameterList = ArrayList<Parameter>()
+        parameterList.add(parameter)
+        return Docket(DocumentationType.SWAGGER_2)
+                .globalOperationParameters(parameterList)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(javaClass.getPackage().name))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo())
+    }
+
+    private fun getApiInfo(): ApiInfo {
+        return ApiInfo("Spring Security Jwt",
+                "A stateless web application with Spring security & Jwt",
+                "1.0", "", Contact("","",""), "", "", listOf())
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<SpringSecurityJwtApplication>(*args)
+    runApplication<SpringSecurityJwtApplication>(*args)
+}
+
+@Controller
+@ApiIgnore
+class HomeController {
+	@RequestMapping("/")
+	fun index(): String {
+		return "redirect:swagger-ui.html"
+	}
+}
+
+@RestController
+class DemoController {
+	@GetMapping("/demo")
+	fun index(): String {
+		return "demo"
+	}
 }
